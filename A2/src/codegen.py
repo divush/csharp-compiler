8bfdb04fb5ea0b82d87b57ac25de132d8c7bf427
Divyanshu Shende <divush@cse.iitk.ac.in>
2016-01-29 03:13:28 +0530

    Made directory for assignment 2

---
+++ A2/src/codegen.py
@@ -0,0 +1,5 @@
+import sys
+#list of registers. Saves work later
+reglist=['rax', ' rbx', ' rcx', ' rdx', ' rbp', ' rsp', ' rsi', ' rdi', ' r8', ' r9', ' r10', ' r11', ' r12', ' r13', ' r14', ' r15']
+#dictionary storing register values.
+registers.fromkeys(reglist)
