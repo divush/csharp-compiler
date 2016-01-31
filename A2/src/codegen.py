83a1f7b388f76b4867c0d7093e1af9a1f90b6918
Divyanshu Shende <divush@cse.iitk.ac.in>
2016-01-31 23:27:39 +0530

    Added some correction to codegen.py and added stuff/works

--- A2/src/codegen.py
+++ A2/src/codegen.py
@@ -1,6 +1,7 @@
 import sys
 #list of registers. Saves work later
 reglist=['rax', ' rbx', ' rcx', ' rdx', ' rbp', ' rsp', ' rsi', ' rdi', ' r8', ' r9', ' r10', ' r11', ' r12', ' r13', ' r14', ' r15']
+registers={}
 registers.fromkeys(reglist)			#dictionary storing register values.
 #symbol table starts out empty.
 symboltable=[]
