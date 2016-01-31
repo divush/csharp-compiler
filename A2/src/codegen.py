4c977ba45fcccb20c6423e4ddd427ac0f362d87e
Divyanshu Shende <divush@cse.iitk.ac.in>
2016-01-31 21:55:06 +0530

    Added symbol table array.

--- A2/src/codegen.py
+++ A2/src/codegen.py
@@ -1,5 +1,6 @@
 import sys
 #list of registers. Saves work later
 reglist=['rax', ' rbx', ' rcx', ' rdx', ' rbp', ' rsp', ' rsi', ' rdi', ' r8', ' r9', ' r10', ' r11', ' r12', ' r13', ' r14', ' r15']
-#dictionary storing register values.
-registers.fromkeys(reglist)
+registers.fromkeys(reglist)			#dictionary storing register values.
+#symbol table starts out empty.
+symboltable=[]
