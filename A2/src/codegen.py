c7036f23793a8e426f6fb2446f050be1078b8fa4
Pranshu Gupta <pranshug@iitk.ac.in>
2016-02-01 20:44:35 +0530

    Added code to supply ir filename from command line

--- A2/src/codegen.py
+++ A2/src/codegen.py
@@ -1,7 +1,27 @@
+# IMPLEMENTING C SHARP IN PYTHON
+# Machine Code Generation Module
+###################################################################################################
+
 import sys
-#list of registers. Saves work later
-reglist=['rax', ' rbx', ' rcx', ' rdx', ' rbp', ' rsp', ' rsi', ' rdi', ' r8', ' r9', ' r10', ' r11', ' r12', ' r13', ' r14', ' r15']
-registers={}
-registers.fromkeys(reglist)			#dictionary storing register values.
-#symbol table starts out empty.
-symboltable=[]
+
+###################################################################################################
+
+if len(sys.argv) == 2:
+	filename = str(sys.argv[1])
+else:
+	print "usage: python codegen.py irfile"
+	exit()
+
+# List of registers
+reglist = ['rax','rbx','rcx','rdx','rbp','rsp','rsi','rdi','r8','r9','r10','r11','r12','r13','r14','r15']
+
+# Dictionary that stores the current values in the register
+registers = {}
+regdict = registers.fromkeys(reglist)
+
+# Load the intermediate representation of the program from a file
+irfile = open(filename, 'r')
+ircode = irfile.read()
+ircode = ircode.strip('\n')
+
+# Construct the basic blocks from ircode
