3128df547cc641a56b809b2eefa6ca25e8fbaa67
Pranshu Gupta <pranshug@iitk.ac.in>
2016-02-01 21:35:24 +0530

    Added outline for getreg function

--- A2/src/codegen.py
+++ A2/src/codegen.py
@@ -6,16 +6,34 @@ import sys
 
 ###################################################################################################
 
+# Function definitions ----------------------------------------------------------------------------
+
+# getreg: returns a register which is currently empty
+def getreg():
+	# Check the current instruction to see if a register is going to be freed
+
+	# Otherwise return an empty register (if available)
+
+	# Otherwise free a register by splitting live range (heuristic algorithm) of a variable
+
+	# Otherwise return None so that memory is allocated instead of a register
+	return None
+
+
+
+#--------------------------------------------------------------------------------------------------
+
+# Get the intermediate code file name
 if len(sys.argv) == 2:
 	filename = str(sys.argv[1])
 else:
 	print "usage: python codegen.py irfile"
 	exit()
 
-# List of registers
+# Define the list of registers
 reglist = ['rax','rbx','rcx','rdx','rbp','rsp','rsi','rdi','r8','r9','r10','r11','r12','r13','r14','r15']
 
-# Dictionary that stores the current values in the register
+# Construct the register descriptor table
 registers = {}
 regdict = registers.fromkeys(reglist)
 
