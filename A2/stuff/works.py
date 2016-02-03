447143e7767e9042b16b4c03ae58e1808b26a553
Divyanshu Shende <divush@cse.iitk.ac.in>
2016-02-03 23:22:28 +0530

    Instruction skeleton added to works.py

--- A2/stuff/works.py
+++ A2/stuff/works.py
@@ -20,5 +20,15 @@ for l in labels:
 nodes=nodes[1:]
 
 
+tackeywords = ['ifgoto', 'goto', 'ret', 'call', 'print', 'label', 'leq', 'geq', '='] + mathops
+newinstr = Instruction(instrlist[i])
 #Sample class declaration
-class 
+class Instruction:
+	"""Instruction class"""
+	def __init__(self, instr):
+		components = instr.split(',')
+		number = components[0]
+		for keyword in tackeywords:
+			if keyword in components:
+				mytype = keyword
+				break;
