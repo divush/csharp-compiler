32eaa4f8dd7edf034a21bffb47b6045e17fae8c8
Pranshu Gupta <pranshug@iitk.ac.in>
2016-02-28 15:37:13 +0530

    Added the opt rule craetion function definition for the non terminals which can go to epsilon

--- A3/src/parser.py
+++ A3/src/parser.py
@@ -14,11 +14,25 @@ else:
 	print("Usage: ./parser file.cs")
 	exit(0)
 
+# Read the input program
 inputfile = open(filename, 'r')
 data = inputfile.read()
 lexer.input(data)
+# Now we can access the tokens in the program as lexer.token()
 
 ###################################################################################################
+
+def create_opt_rules(rulename):
+	optname = rulename + '_opt'
+	def optrule(self, p):
+	    p[0] = p[1]
+	optrule.__doc__ = '%s : empty\n| %s' % (optname, rulename)
+	optrule.__name__ = 'p_%s' % optname
+	setattr(self.__class__, optrule.__name__, optrule)
+
+
+###################################################################################################
+
 
 # Precedence and associativity of operators
 precedence = (
@@ -34,4 +48,14 @@ precedence = (
 	('left', 'TIMES', 'DIVIDE', 'MOD')
 )
 
+opt_rules = [
+	''
+]
+
+for rule in opt_rules:
+	create_opt_rule(rule)
+
 # Grammar Productions for C#
+def p_compilation_unit(p):
+	"""
+	"""
