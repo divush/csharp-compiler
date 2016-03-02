bb29459a272348294372c0ce2bef3d9a784655ad
Pranshu Gupta <pranshug@iitk.ac.in>
2016-03-02 19:33:56 +0530

    Added the list of the cfg variables for which we have to create empty productions, commented in parser.py

--- A3/src/parser.py
+++ A3/src/parser.py
@@ -17,20 +17,62 @@ else:
 # Read the input program
 inputfile = open(filename, 'r')
 data = inputfile.read()
-lexer.input(data)
+# lexer.input(data)
 # Now we can access the tokens in the program as lexer.token()
 
 ###################################################################################################
 
-
-def create_opt_rules(rulename):
-	optname = rulename + '_opt'
-
-	def optrule(self, p):
-	    p[0] = p[1]
-	optrule.__doc__ = '%s : empty\n| %s' % (optname, rulename)
-	optrule.__name__ = 'p_%s' % optname
-	setattr(self.__class__, optrule.__name__, optrule)
+# ADD OPT RULES HERE (CFG VARIABLES THAT HAVE EMPTY PRODUCTIONS)
+# implicit_anonymous_function_parameter_list_opt
+# generic_dimension_specifier_opt
+# method_modifiers_opt
+# variant_type_parameter_list_opt
+# formal_parameter_list_opt
+# get_accessor_declaration_opt
+# indexer_modifiers_opt
+# rank_specifiers_opt
+# STMT_TERMINATOR_opt
+# enum_member_declarations_opt
+# for_initializer_opt
+# constant_modifiers_opt
+# set_accessor_declaration_opt
+# enum_modifiers_opt
+# enum_base_opt
+# default_argument_opt
+# array_initializer_opt
+# struct_modifiers_opt
+# accessor_modifier_opt
+# switch_sections_opt
+# namespace_member_declarations_opt
+# explicit_anonymous_function_signature_opt
+# property_modifiers_opt
+# parameter_modifier_opt
+# field_modifiers_opt
+# variable_initializer_list_opt
+# member_declarator_list_opt
+# "partial"_opt
+# class_base_opt
+# explicit_anonymous_function_parameter_list_opt
+# argument_name_opt
+# type_parameter_constraints_clauses_opt
+# statement_list_opt
+# for_condition_opt
+# struct_member_declarations_opt
+# class_modifiers_opt
+# expression_opt
+# delegate_modifiers_opt
+# for_iterator_opt
+# object_or_collection_initializer_opt
+# event_modifiers_opt
+# dim_separators_opt
+# constructor_initializer_opt
+# member_initializer_list_opt
+# using_directives_opt
+# commas_opt
+# class_member_declarations_opt
+# constructor_modifiers_opt
+# EXTERN_opt
+# argument_list_opt
 
 
 ###################################################################################################
