51fbf56f21c0616257cc551e6b4a1ed48f2e8326
Pranshu Gupta <pranshug@iitk.ac.in>
2016-04-04 09:28:39 +0530

    Started working on irgen.py, symbol table and the functions used in the parser will be implemented here

---
+++ A4/src/irgen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Parser for C# in Python
+
+# Grammar Specification Reference
+# https://msdn.microsoft.com/en-in/library/aa664812(v=vs.71).aspx
+# With some modifications to resolve conflicts
+
+# Authors: Divyanshu Shende, Pranshu Gupta, Prashant Kumar, Rahul Tudu
+# Compiler Design: CS335A, Group 25, Indian Institute of Technology, Kanpur
+###################################################################################################
+
+from lexer import *
+
+###################################################################################################
+
+# Implement the symbol table and all the IR methods here
+
