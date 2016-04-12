3fc68ac6248419871808b0768203ef8a45acfccf
Pranshu Gupta <pranshug258@outlook.com>
2016-04-12 15:33:04 +0530

    Cleaned up

--- A3/test/test6.cs
+++ A3/test/test6.cs
@@ -1,11 +1,4 @@
-/*
- * C# Program to Get a Number and Display the Sum of the Digits 
- */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
- 
+using System; 
 namespace Program
 {
     class Program
@@ -13,8 +6,7 @@ namespace Program
         static void Main(string[] args)
         {
             int num;
-	num =  2 +
- 
+			num =  2 + 3;
         }
     }
 }
